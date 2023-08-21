281c3e5533b264b6c7d23643ed38ff0aa2b9efcd
Marko Sarcevic <sarcevic.m@outlook.com>
2023-08-21 15:44:57 -0700

    Save long data into database. Created new php file.

--- index.php
+++ index.php
@@ -12,6 +12,26 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 switch($method) {
 
+    case 'GET':
+
+        $sql = "SELECT * FROM german_english_shorttext";
+        $path = explode('/', $_SERVER['REQUEST_URI'] ); 
+
+        if (isset($path[3]) && is_numeric($path[3])) {
+            $sql .= " WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $path[3]);
+            $stmt->execute();
+            $users = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        echo json_encode($users);
+        break;
+
 
     case 'POST':
 
---
+++ save_longText.php
@@ -0,0 +1,41 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Methods: *');
+
+include 'DB_connection.php';
+$objDb = new DbConnect;
+$conn = $objDb->connect();
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'POST':
+        $data = json_decode(file_get_contents('php://input'));
+
+        if ($data !== null) {
+            // Assuming 'text_data' is the name of the column in your table.
+            $sql = "INSERT INTO german_grammar (text_data) VALUES (:textData)";
+            
+            $stmt = $conn->prepare($sql);
+
+            // Bind the parameter
+            $stmt->bindParam(':textData', $data->text_data);
+
+            if ($stmt->execute()) {
+                // Data was successfully inserted
+                $response = ['message' => 'Data inserted successfully'];
+                echo json_encode($response);
+            } else {
+                // Handle the case where the insertion failed
+                $response = ['error' => 'Failed to insert data'];
+                echo json_encode($response);
+            }
+        } else {
+            // Handle the case where JSON parsing failed
+            $response = ['error' => 'Invalid JSON data'];
+            echo json_encode($response);
+        }
+        break;
+
+}
